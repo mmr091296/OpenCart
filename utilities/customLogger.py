3793cceb7eae1326fbc3b26498aaadf143ccb555
mmr091296 <mmr091296@gmail.com>
2024-05-30 16:28:23 +0530

    Tewst

--- utilities/customLogger.py
+++ utilities/customLogger.py
@@ -1,17 +1,31 @@
+# import logging
+# import os
+#
+# class LogGen:
+#     @staticmethod
+#     def loggen():
+#         path = (os.path.dirname(os.getcwd())+"\\OpenCart\\logs\\automation.log")
+#         logger = logging.getLogger(__name__)  # Create or get a logger with the name of the current module.
+#         logger.setLevel(logging.DEBUG)
+#         formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
+#                                       datefmt='%m/%d/%Y %I:%M:%S %p')
+#         file_handler = logging.FileHandler(path)  # Create a file handler to write log messages.
+#         file_handler.setFormatter(formatter)  # Set the ]formatter for the file handler.
+#         logger.addHandler(file_handler)  # Add the file handler to the logger.
+#         return logger  # Return the configured logger.
+
+
+
 import logging
 import os
 
-class LogGen:
+class LogGen():
     @staticmethod
     def loggen():
-        path = (os.path.dirname(os.getcwd())+"\\OpenCart\\logs\\automation.log")
-        logger = logging.getLogger(__name__)  # Create or get a logger with the name of the current module.
+        path = os.path.abspath(os.curdir) + '\\logs\\automation.log'
+        logging.basicConfig(filename=path,
+                            format='%(asctime)s: %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s: %(levelname)s: %(message)s',
-                                      datefmt='%m/%d/%Y %I:%M:%S %p')
-        file_handler = logging.FileHandler(path)  # Create a file handler to write log messages.
-        file_handler.setFormatter(formatter)  # Set the formatter for the file handler.
-        logger.addHandler(file_handler)  # Add the file handler to the logger.
-        return logger  # Return the configured logger.
-
+        return logger
 
